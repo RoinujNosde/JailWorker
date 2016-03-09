00bed3915a231c2209c5b67971f1f64734844a49
Edson Passos <RoinujNosde@users.noreply.github.com>
2016-03-08 22:16:08 -0300

    Update JWOnAuthMeLoginEvent.java

--- src/fr/alienationgaming/jailworker/listner/JWOnAuthMeLoginEvent.java
+++ src/fr/alienationgaming/jailworker/listner/JWOnAuthMeLoginEvent.java
@@ -28,5 +28,47 @@ public class JWOnAuthMeLoginEvent implements Listener {
 			player.sendMessage(plugin.toLanguage("info-command-prisonerorder", remain, plugin.getJailConfig().getString("Jails." + jailName + ".Type")));
 			player.teleport(new Location(world, spawn.getX(), spawn.getY()+1, spawn.getZ()));
 		}
+		if (plugin.getJailConfig().contains("Queue." + player.getName())) {
+			String jailName = plugin.getJailConfig().getString("Queue." + player.getName() + ".Prison");
+			String Punisher = plugin.getJailConfig().getString("Queue." + player.getName() + ".Punisher");
+			int blocks = plugin.getJailConfig().getInt("Queue." + player.getName() + ".PunishToBreak");
+			String cause = plugin.getJailConfig().getString("Queue." + player.getName() + ".Cause");
+			
+			/* Get inventory */
+			JWInventorySaver invSaver = new JWInventorySaver(plugin);
+			
+			/* Create Section prisoner for target */
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".Prison", jailName);
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".Punisher", Punisher);
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".Date", utils.getDate());
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".PreviousPosition", player.getLocation().toVector());
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".PreviousWorld", player.getWorld().getName());
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".PunishToBreak", blocks);
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".RemainingBlocks", blocks);
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".Cause", cause);
+			plugin.getJailConfig().set("Prisoners." + player.getName() + ".Gamemode", player.getGameMode().name());
+			if (player.getGameMode() == GameMode.CREATIVE)
+				player.setGameMode(GameMode.SURVIVAL);
+			invSaver.save(player);
+			invSaver.clear(player);
+
+			/* Send Player to jail */
+			player.getInventory().clear();
+			player.getEquipment().clear();
+			Vector spawn = plugin.getJailConfig().getVector("Jails." + jailName + ".Location.PrisonerSpawn");
+			World world = plugin.getServer().getWorld(plugin.getJailConfig().getString("Jails." + jailName + ".World"));
+			player.teleport(new Location(world, spawn.getX(), spawn.getY()+1, spawn.getZ()));
+			plugin.getServer().broadcastMessage(plugin.toLanguage("info-command-broadcastpunish", player.getName(), jailName, Punisher));
+			plugin.getServer().broadcastMessage(plugin.toLanguage("info-command-broadcastcantear"));
+			player.sendMessage(plugin.toLanguage("info-command-sendtojail", Punisher));
+			if (!cause.isEmpty() && cause != "No Reason.")
+				player.sendMessage(plugin.toLanguage("info-command-displayreason", cause));
+			player.sendMessage(plugin.toLanguage("info-command-prisonerorder", blocks, plugin.getJailConfig().getString("Jails." + jailName + ".Type")));
+			
+			plugin.getJailConfig().set("Queue." + player.getName(), null);
+			plugin.saveJailConfig();
+			plugin.reloadJailConfig();
+			
+		}
 	}
 }
